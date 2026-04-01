9bc05211d6b0baf4ed2970a4e1dd19b6cefbdde5
Omar Al Himsi <60303391@udst.edu.qa>
2026-04-01 21:54:39 +0300

    fix: Lint test not passing, checked for errors. (#25)

--- main/app.py
+++ main/app.py
@@ -6,25 +6,25 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-		return """
-		<!doctype html>
-		<html lang="en">
-			<head>
-				<meta charset="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>VitalAI</title>
-			</head>
-			<body>
-				<h1>VitalAI</h1>
-				<p>
-					<a href="/health">Health Check</a>
-				</p>
-			</body>
-		</html>
-		"""
+    return """
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>VitalAI</title>
+        </head>
+        <body>
+            <h1>VitalAI</h1>
+            <p>
+                <a href="/health">Health Check</a>
+            </p>
+        </body>
+    </html>
+    """
 
 
 @app.get("/health")
 def health():
-		return jsonify({"status": "OK"}), 200
+    return jsonify({"status": "OK"}), 200
 
